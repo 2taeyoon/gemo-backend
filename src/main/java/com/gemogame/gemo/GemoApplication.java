c4c6de6bbc992784b58a3aa33b063aeb64cf57f0
2taeyoon <2tae@kakao.com>
2025-05-28 23:32:06 +0900

    setting

---
+++ src/main/java/com/gemogame/gemo/GemoApplication.java
@@ -0,0 +1,13 @@
+package com.gemogame.gemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemogame/gemo/GemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemogame.gemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
