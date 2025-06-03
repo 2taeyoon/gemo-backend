e94054437759c65d2b067f0240d9805c833127d3
2taeyoon <2tae@kakao.com>
2025-06-04 00:32:08 +0900

    테스트 컨트롤러 레포지토리 엔티티 추가

--- src/main/java/com/gemogame/gemo/GemoApplication.java
+++ src/main/java/com/gemogame/gemo/GemoApplication.java
@@ -1,13 +1,27 @@
 package com.gemogame.gemo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class GemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GemoApplication.class, args);
-	}
+		// 1. .env 강제 로딩
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+		// 2. 로딩된 환경변수 → Spring 초기 환경에 주입
+		Map<String, Object> props = new HashMap<>();
+		props.put("DB_URL", dotenv.get("DB_URL"));
+		props.put("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		props.put("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication app = new SpringApplication(GemoApplication.class);
+		app.setDefaultProperties(props); // 이게 핵심!
+		app.run(args);
+	}
 }
